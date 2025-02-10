974fa29c554f9f8006284fb6f0c9dc09e23927c2
Literary and Debating Club, NITC <90113700+LnD-NITC@users.noreply.github.com>
2025-02-10 18:07:48 +0530

    Update Footer.jsx

--- App/frontend/BookMate/src/components/Footer/Footer.jsx
+++ App/frontend/BookMate/src/components/Footer/Footer.jsx
@@ -1,33 +1,36 @@
 import React from "react";
 import githubLogo from "../../assets/icons8-github.svg";
+import lndLogo from "../../assets/lndlogo.png";
+import web from "../../assets/web.png";
+import instagram from "../../assets/instagram.svg";
 import arunnats from "../../assets/Logo_arunnats.png";
 import linkedinLogo from "../../assets/icons8-linkedin.svg";
 
 const Footer = () => {
-	return (
-		<footer className="footer items-center p-2 bg-neutral text-neutral-content">
-			<div className="items-center grid-flow-col">
-				<p>Book Mate - By Arun Natarajan</p>
-			</div>
-			<nav className="grid-flow-col gap-4 md:place-self-center md:justify-self-end">
-				<a className="btn btn-ghost" href="https://www.arunnats.com/">
-					<img className=" h-8 max-w-lg" src={arunnats} alt="" />
-				</a>
-				<a
-					href="https://www.linkedin.com/in/arun-natarajan-567539211//"
-					className="btn btn-ghost text-xl"
-				>
-					<img className=" h-8 max-w-lg" src={linkedinLogo} alt="Logo" />
-				</a>
-				<a
-					href="https://github.com/arunnats//"
-					className="btn btn-ghost text-xl"
-				>
-					<img className=" h-8 max-w-lg" src={githubLogo} alt="Logo" />
-				</a>
-			</nav>
-		</footer>
-	);
+  return (
+    <footer className="footer items-center p-2 bg-neutral text-neutral-content">
+      <div className="items-center grid-flow-col">
+        <p>Book Mate - By LnD NITC</p>
+        <a
+          href="https://github.com/arunnats/"
+          className="btn btn-ghost text-xl"
+        >
+          <img className=" h-8 max-w-lg" src={githubLogo} alt="Logo" />
+        </a>
+      </div>
+      <nav className="grid-flow-col md:place-self-center md:justify-self-end">
+        <a href="https://lndnitc.org/" className="btn btn-ghost text-xl">
+          <img className=" h-8 max-w-lg" src={web} alt="Logo" />
+        </a>
+        <a
+          href="https://www.instagram.com/donttellthewarden/"
+          className="btn btn-ghost text-xl"
+        >
+          <img className=" h-8 max-w-lg" src={instagram} alt="Logo" />
+        </a>
+      </nav>
+    </footer>
+  );
 };
 
 export default Footer;
